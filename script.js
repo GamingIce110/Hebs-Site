e00418e21e7b9d514426cd71982d312b52737eea
GamingIce110 <165563519+GamingIce110@users.noreply.github.com>
2025-08-01 16:35:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-const members = {
-  "Leader541": "Shayan",
-  "Hacker427": "Qaim",
-  "exe829": "Hasan",
-  "fighter,7648": "Hadi",
-  "spy784": "Ayan"
-};
+// script.js const passwords = { "Leader541": "Shayan", "Hacker427": "Qaim", "exe829": "Hasan", "fighter,7648": "Hadi", "spy784": "Ayan" };
 
-function login() {
-  const pass = document.getElementById("password").value;
-  const member = members[pass];
-  const status = document.getElementById("status");
+function login() { const pass = document.getElementById("passwordInput").value; const name = passwords[pass];
 
-  if (member) {
-    localStorage.setItem("hebsUser", member);
-    window.location.href = "main.html";
-  } else {
-    status.innerText = "Wrong password!";
-  }
-}
+if (name) { localStorage.setItem("hebsMember", name); Swal.fire({ title: Access Granted , text: Welcome ${name}, icon: "success", timer: 1500, showConfirmButton: false }).then(() => { window.location.href = "dashboard.html"; }); } else { Swal.fire({ title: "Access Denied", text: "Wrong password", icon: "error", timer: 1500, showConfirmButton: false }); } }
+
