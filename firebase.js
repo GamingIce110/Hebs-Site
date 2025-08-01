a4ff9d34322717769546621175552d9bfcb01977
GamingIce110 <165563519+GamingIce110@users.noreply.github.com>
2025-08-01 15:56:01 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDc4txFkjXsN6ds8bZannRwECTR1T-Y1Z4",
+  authDomain: "hebs-3daa8.firebaseapp.com",
+  databaseURL: "https://hebs-3daa8-default-rtdb.firebaseio.com",
+  projectId: "hebs-3daa8",
+  storageBucket: "hebs-3daa8.firebasestorage.app",
+  messagingSenderId: "401317379209",
+  appId: "1:401317379209:web:48ff3bad41915e499e1a58"
+};
+firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-js
+const members = {
+  "Leader541": "Shayan",
+  "Hacker427": "Qaim",
+  "exe829": "Hasan",
+  "fighter,7648": "Hadi",
+  "spy784": "Ayan"
+};
+
+function login() {
+  const pass = document.getElementById("password").value;
+  const member = members[pass];
+  const status = document.getElementById("status");
+
+  if (member) {
+    localStorage.setItem("hebsUser", member);
+    window.location.href = "main.html";
+  } else {
+    status.innerText = "Wrong password!";
+  }
+}
