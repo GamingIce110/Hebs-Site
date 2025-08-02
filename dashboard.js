85880d85b381a0fa3f80da7cab6fce667523361f
GamingIce110 <165563519+GamingIce110@users.noreply.github.com>
2025-08-02 19:20:22 +0500

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,15 @@
+function openChat() {
+  window.location.href = "chat.html";
+}
+
+function openMissions() {
+  window.location.href = "mission.html";
+}
+
+function openVault() {
+  window.location.href = "vault.html";
+}
+
+function openTerminal() {
+  window.location.href = "terminal.html";
+}
